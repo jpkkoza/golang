4f27ac16624877961061b6652f9019045f16ccbd
jpkkoza <jpkkoza26@gmail.com>
2025-04-16 11:26:44 +0500

    фикс makefile

--- internal/web/tasks/api.gen.go
+++ internal/web/tasks/api.gen.go
@@ -8,7 +8,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/oapi-codegen/runtime"
@@ -22,27 +21,11 @@ type Task struct {
 	Task   *string `json:"task,omitempty"`
 }
 
-// User defines model for User.
-type User struct {
-	CreatedAt *time.Time `json:"created_at,omitempty"`
-	DeletedAt *time.Time `json:"deleted_at"`
-	Email     *string    `json:"email,omitempty"`
-	Id        *uint      `json:"id,omitempty"`
-	Password  *string    `json:"password,omitempty"`
-	UpdatedAt *time.Time `json:"updated_at,omitempty"`
-}
-
 // PostTasksJSONRequestBody defines body for PostTasks for application/json ContentType.
 type PostTasksJSONRequestBody = Task
 
 // PatchTasksIdJSONRequestBody defines body for PatchTasksId for application/json ContentType.
 type PatchTasksIdJSONRequestBody = Task
-
-// PostUsersJSONRequestBody defines body for PostUsers for application/json ContentType.
-type PostUsersJSONRequestBody = User
-
-// PatchUsersIdJSONRequestBody defines body for PatchUsersId for application/json ContentType.
-type PatchUsersIdJSONRequestBody = User
 
 // ServerInterface represents all server handlers.
 type ServerInterface interface {
@@ -58,18 +41,6 @@ type ServerInterface interface {
 	// Обновить задачу по ID
 	// (PATCH /tasks/{id})
 	PatchTasksId(ctx echo.Context, id int) error
-	// Get all users
-	// (GET /users)
-	GetUsers(ctx echo.Context) error
-	// Create a new user
-	// (POST /users)
-	PostUsers(ctx echo.Context) error
-	// Удалить пользователя по ID
-	// (DELETE /users/{id})
-	DeleteUsersId(ctx echo.Context, id int) error
-	// Обновить пользователя по ID
-	// (PATCH /users/{id})
-	PatchUsersId(ctx echo.Context, id int) error
 }
 
 // ServerInterfaceWrapper converts echo contexts to parameters.
@@ -127,56 +98,6 @@ func (w *ServerInterfaceWrapper) PatchTasksId(ctx echo.Context) error {
 	return err
 }
 
-// GetUsers converts echo context to params.
-func (w *ServerInterfaceWrapper) GetUsers(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshaled arguments
-	err = w.Handler.GetUsers(ctx)
-	return err
-}
-
-// PostUsers converts echo context to params.
-func (w *ServerInterfaceWrapper) PostUsers(ctx echo.Context) error {
-	var err error
-
-	// Invoke the callback with all the unmarshaled arguments
-	err = w.Handler.PostUsers(ctx)
-	return err
-}
-
-// DeleteUsersId converts echo context to params.
-func (w *ServerInterfaceWrapper) DeleteUsersId(ctx echo.Context) error {
-	var err error
-	// ------------- Path parameter "id" -------------
-	var id int
-
-	err = runtime.BindStyledParameterWithLocation("simple", false, "id", runtime.ParamLocationPath, ctx.Param("id"), &id)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Invalid format for parameter id: %s", err))
-	}
-
-	// Invoke the callback with all the unmarshaled arguments
-	err = w.Handler.DeleteUsersId(ctx, id)
-	return err
-}
-
-// PatchUsersId converts echo context to params.
-func (w *ServerInterfaceWrapper) PatchUsersId(ctx echo.Context) error {
-	var err error
-	// ------------- Path parameter "id" -------------
-	var id int
-
-	err = runtime.BindStyledParameterWithLocation("simple", false, "id", runtime.ParamLocationPath, ctx.Param("id"), &id)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Invalid format for parameter id: %s", err))
-	}
-
-	// Invoke the callback with all the unmarshaled arguments
-	err = w.Handler.PatchUsersId(ctx, id)
-	return err
-}
-
 // This is a simple interface which specifies echo.Route addition functions which
 // are present on both echo.Echo and echo.Group, since we want to allow using
 // either of them for path registration
@@ -209,10 +130,6 @@ func RegisterHandlersWithBaseURL(router EchoRouter, si ServerInterface, baseURL 
 	router.POST(baseURL+"/tasks", wrapper.PostTasks)
 	router.DELETE(baseURL+"/tasks/:id", wrapper.DeleteTasksId)
 	router.PATCH(baseURL+"/tasks/:id", wrapper.PatchTasksId)
-	router.GET(baseURL+"/users", wrapper.GetUsers)
-	router.POST(baseURL+"/users", wrapper.PostUsers)
-	router.DELETE(baseURL+"/users/:id", wrapper.DeleteUsersId)
-	router.PATCH(baseURL+"/users/:id", wrapper.PatchUsersId)
 
 }
 
@@ -315,105 +232,6 @@ func (response PatchTasksId500Response) VisitPatchTasksIdResponse(w http.Respons
 	return nil
 }
 
-type GetUsersRequestObject struct {
-}
-
-type GetUsersResponseObject interface {
-	VisitGetUsersResponse(w http.ResponseWriter) error
-}
-
-type GetUsers200JSONResponse []User
-
-func (response GetUsers200JSONResponse) VisitGetUsersResponse(w http.ResponseWriter) error {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-
-	return json.NewEncoder(w).Encode(response)
-}
-
-type PostUsersRequestObject struct {
-	Body *PostUsersJSONRequestBody
-}
-
-type PostUsersResponseObject interface {
-	VisitPostUsersResponse(w http.ResponseWriter) error
-}
-
-type PostUsers201JSONResponse User
-
-func (response PostUsers201JSONResponse) VisitPostUsersResponse(w http.ResponseWriter) error {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(201)
-
-	return json.NewEncoder(w).Encode(response)
-}
-
-type DeleteUsersIdRequestObject struct {
-	Id int `json:"id"`
-}
-
-type DeleteUsersIdResponseObject interface {
-	VisitDeleteUsersIdResponse(w http.ResponseWriter) error
-}
-
-type DeleteUsersId204Response struct {
-}
-
-func (response DeleteUsersId204Response) VisitDeleteUsersIdResponse(w http.ResponseWriter) error {
-	w.WriteHeader(204)
-	return nil
-}
-
-type DeleteUsersId400Response struct {
-}
-
-func (response DeleteUsersId400Response) VisitDeleteUsersIdResponse(w http.ResponseWriter) error {
-	w.WriteHeader(400)
-	return nil
-}
-
-type DeleteUsersId500Response struct {
-}
-
-func (response DeleteUsersId500Response) VisitDeleteUsersIdResponse(w http.ResponseWriter) error {
-	w.WriteHeader(500)
-	return nil
-}
-
-type PatchUsersIdRequestObject struct {
-	Id   int `json:"id"`
-	Body *PatchUsersIdJSONRequestBody
-}
-
-type PatchUsersIdResponseObject interface {
-	VisitPatchUsersIdResponse(w http.ResponseWriter) error
-}
-
-type PatchUsersId200JSONResponse User
-
-func (response PatchUsersId200JSONResponse) VisitPatchUsersIdResponse(w http.ResponseWriter) error {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-
-	return json.NewEncoder(w).Encode(response)
-}
-
-type PatchUsersId400Response struct {
-}
-
-func (response PatchUsersId400Response) VisitPatchUsersIdResponse(w http.ResponseWriter) error {
-	w.WriteHeader(400)
-	return nil
-}
-
-type PatchUsersId500Response struct {
-}
-
-func (response PatchUsersId500Response) VisitPatchUsersIdResponse(w http.ResponseWriter) error {
-	w.WriteHeader(500)
-	return nil
-}
-
 // StrictServerInterface represents all server handlers.
 type StrictServerInterface interface {
 	// Get all tasks
@@ -428,18 +246,6 @@ type StrictServerInterface interface {
 	// Обновить задачу по ID
 	// (PATCH /tasks/{id})
 	PatchTasksId(ctx context.Context, request PatchTasksIdRequestObject) (PatchTasksIdResponseObject, error)
-	// Get all users
-	// (GET /users)
-	GetUsers(ctx context.Context, request GetUsersRequestObject) (GetUsersResponseObject, error)
-	// Create a new user
-	// (POST /users)
-	PostUsers(ctx context.Context, request PostUsersRequestObject) (PostUsersResponseObject, error)
-	// Удалить пользователя по ID
-	// (DELETE /users/{id})
-	DeleteUsersId(ctx context.Context, request DeleteUsersIdRequestObject) (DeleteUsersIdResponseObject, error)
-	// Обновить пользователя по ID
-	// (PATCH /users/{id})
-	PatchUsersId(ctx context.Context, request PatchUsersIdRequestObject) (PatchUsersIdResponseObject, error)
 }
 
 type StrictHandlerFunc = strictecho.StrictEchoHandlerFunc
@@ -556,114 +362,6 @@ func (sh *strictHandler) PatchTasksId(ctx echo.Context, id int) error {
 		return err
 	} else if validResponse, ok := response.(PatchTasksIdResponseObject); ok {
 		return validResponse.VisitPatchTasksIdResponse(ctx.Response())
-	} else if response != nil {
-		return fmt.Errorf("unexpected response type: %T", response)
-	}
-	return nil
-}
-
-// GetUsers operation middleware
-func (sh *strictHandler) GetUsers(ctx echo.Context) error {
-	var request GetUsersRequestObject
-
-	handler := func(ctx echo.Context, request interface{}) (interface{}, error) {
-		return sh.ssi.GetUsers(ctx.Request().Context(), request.(GetUsersRequestObject))
-	}
-	for _, middleware := range sh.middlewares {
-		handler = middleware(handler, "GetUsers")
-	}
-
-	response, err := handler(ctx, request)
-
-	if err != nil {
-		return err
-	} else if validResponse, ok := response.(GetUsersResponseObject); ok {
-		return validResponse.VisitGetUsersResponse(ctx.Response())
-	} else if response != nil {
-		return fmt.Errorf("unexpected response type: %T", response)
-	}
-	return nil
-}
-
-// PostUsers operation middleware
-func (sh *strictHandler) PostUsers(ctx echo.Context) error {
-	var request PostUsersRequestObject
-
-	var body PostUsersJSONRequestBody
-	if err := ctx.Bind(&body); err != nil {
-		return err
-	}
-	request.Body = &body
-
-	handler := func(ctx echo.Context, request interface{}) (interface{}, error) {
-		return sh.ssi.PostUsers(ctx.Request().Context(), request.(PostUsersRequestObject))
-	}
-	for _, middleware := range sh.middlewares {
-		handler = middleware(handler, "PostUsers")
-	}
-
-	response, err := handler(ctx, request)
-
-	if err != nil {
-		return err
-	} else if validResponse, ok := response.(PostUsersResponseObject); ok {
-		return validResponse.VisitPostUsersResponse(ctx.Response())
-	} else if response != nil {
-		return fmt.Errorf("unexpected response type: %T", response)
-	}
-	return nil
-}
-
-// DeleteUsersId operation middleware
-func (sh *strictHandler) DeleteUsersId(ctx echo.Context, id int) error {
-	var request DeleteUsersIdRequestObject
-
-	request.Id = id
-
-	handler := func(ctx echo.Context, request interface{}) (interface{}, error) {
-		return sh.ssi.DeleteUsersId(ctx.Request().Context(), request.(DeleteUsersIdRequestObject))
-	}
-	for _, middleware := range sh.middlewares {
-		handler = middleware(handler, "DeleteUsersId")
-	}
-
-	response, err := handler(ctx, request)
-
-	if err != nil {
-		return err
-	} else if validResponse, ok := response.(DeleteUsersIdResponseObject); ok {
-		return validResponse.VisitDeleteUsersIdResponse(ctx.Response())
-	} else if response != nil {
-		return fmt.Errorf("unexpected response type: %T", response)
-	}
-	return nil
-}
-
-// PatchUsersId operation middleware
-func (sh *strictHandler) PatchUsersId(ctx echo.Context, id int) error {
-	var request PatchUsersIdRequestObject
-
-	request.Id = id
-
-	var body PatchUsersIdJSONRequestBody
-	if err := ctx.Bind(&body); err != nil {
-		return err
-	}
-	request.Body = &body
-
-	handler := func(ctx echo.Context, request interface{}) (interface{}, error) {
-		return sh.ssi.PatchUsersId(ctx.Request().Context(), request.(PatchUsersIdRequestObject))
-	}
-	for _, middleware := range sh.middlewares {
-		handler = middleware(handler, "PatchUsersId")
-	}
-
-	response, err := handler(ctx, request)
-
-	if err != nil {
-		return err
-	} else if validResponse, ok := response.(PatchUsersIdResponseObject); ok {
-		return validResponse.VisitPatchUsersIdResponse(ctx.Response())
 	} else if response != nil {
 		return fmt.Errorf("unexpected response type: %T", response)
 	}
--- makefile
+++ makefile
@@ -19,12 +19,13 @@ migrate-down:
 # для удобства добавим команду run, которая будет запускать наше приложение
 run:
 	go run cmd/app/main.go # Теперь при вызове make run мы запустим наш сервер
-	
-gen:
-	oapi-codegen -config openapi/.openapi -include-tags tasks,users -package tasks openapi/openapi.yaml > ./internal/web/tasks/api.gen.go
 
 lint:
 	golangci-lint run --out-format=colored-line-number
 
+gen-tasks:
+	oapi-codegen -config openapi/.openapi -include-tags tasks -package tasks openapi/openapi.yaml > ./internal/web/tasks/api.gen.go
+
 gen-users:
 	oapi-codegen -config openapi/.openapi -include-tags users -package users openapi/openapi.yaml > ./internal/web/users/api.gen.go
+
